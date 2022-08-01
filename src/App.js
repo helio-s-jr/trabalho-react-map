fc859ef02c815aeaedd5d1908872c9ee01111374
Helio Junior <heliojr.zpt@gmail.com>
2022-08-01 10:48:58 -0300

    Trabalho de map em react finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  state = {
+    amigos:[
+      {
+        Nome: "Marcos",
+        idade: 38,
+        profissao: "Analista de sistemas",
+        descricao: "Atenção ao sistema",
+      },
+      {
+        Nome: "Ivan",
+        idade: 37,
+        profissao: "Biólogo",
+        descricao: "Atenção aos animais",
+      },
+      {
+        Nome: "Renan",
+        idade: 33,
+        profissao: "Artesão",
+        descricao: "Atenção no quadro",
+      },
+      {
+        Nome: "Douglas",
+        idade: 40,
+        profissao: "Motorista",
+        descricao: "Atenção no trânsito",
+      },
+    ]
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Meu amigos</h1>
+
+        <ul>
+          <li>{this.state.amigos[0].Nome}</li>
+          <li>{this.state.amigos[0].idade}</li>
+          <li>{this.state.amigos[0].profissao}</li>
+          <li>{this.state.amigos[0].descricao}</li>
+        </ul>
+
+        <ul>
+          <li>{this.state.amigos[1].Nome}</li>
+          <li>{this.state.amigos[1].idade}</li>
+          <li>{this.state.amigos[1].profissao}</li>
+          <li>{this.state.amigos[1].descricao}</li>
+        </ul>
+
+        <ul>
+          <li>{this.state.amigos[2].Nome}</li>
+          <li>{this.state.amigos[2].idade}</li>
+          <li>{this.state.amigos[2].profissao}</li>
+          <li>{this.state.amigos[2].descricao}</li>
+        </ul>
+
+        <ul>
+          <li>{this.state.amigos[3].Nome}</li>
+          <li>{this.state.amigos[3].idade}</li>
+          <li>{this.state.amigos[3].profissao}</li>
+          <li>{this.state.amigos[3].descricao}</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
